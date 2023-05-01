1f1fd9d4610c7f5f1740855f24214d2bb7e636df
juicetanz <66227184+juicetanz@users.noreply.github.com>
2023-05-01 06:46:55 -0700

    fix menu

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://Level1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
