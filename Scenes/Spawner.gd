508131422c19c04801f2e7bf882acc151f985f70
juicetanz <66227184+juicetanz@users.noreply.github.com>
2023-04-30 21:15:09 -0700

    spawner

---
+++ Scenes/Spawner.gd
@@ -0,0 +1,22 @@
+extends StaticBody2D
+
+const Bullet = preload("res://Scenes/Bullet.tscn")
+var speed = 200
+var velocity = Vector2()
+
+var cooldown = 1
+var time = 0.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time += delta
+	if time > cooldown:
+		time = 0.0
+		
+		shoot()
+
+func shoot():
+	var b = Bullet.instantiate()
+	
+	b.start($Muzzle.global_position, rotation)
+	get_parent().add_child(b)
