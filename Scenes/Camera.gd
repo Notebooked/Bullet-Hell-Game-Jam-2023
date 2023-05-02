05c225ed10c30bc68c74ce039d960bd458c5bff4
Notebooked <creepinglion09@gmail.com>
2023-05-02 03:30:39 -0700

    godot :apple:

--- Scenes/Camera.gd
+++ Scenes/Camera.gd
@@ -1,8 +1,9 @@
 extends Camera3D
 
 var target_position: Vector3
+var target_rotation: Vector3
 
-var position_lerp = 0.125
+var lerp_value = 0.125
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,9 +14,14 @@ func _ready():
 func _process(delta):
 	if Input.is_key_pressed(KEY_1):
 		target_position = Vector3(0,0,25)
+		target_rotation = Vector3(0,0,0)
 	if Input.is_key_pressed(KEY_2):
 		target_position = Vector3(1,1,1).normalized() * 25
+		var angle = rad_to_deg(Vector3(1,1,1).angle_to(Vector3(1,1,0)))
+		target_rotation = Vector3(-angle,45,0)
+	if Input.is_key_pressed(KEY_3):
+		target_position = Vector3(0,25,0)
+		target_rotation = Vector3(-90,0,0)
 	
-	position = lerp(position, target_position, position_lerp)
-	
-	look_at(Vector3.ZERO)
+	position = lerp(position, target_position, lerp_value)
+	rotation_degrees = lerp(rotation_degrees, target_rotation, lerp_value)
