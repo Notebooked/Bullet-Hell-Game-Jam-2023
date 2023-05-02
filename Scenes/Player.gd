91fc8fff4710cee2f5c3ba4281737b9b71301ca5
Notebooked <creepinglion09@gmail.com>
2023-05-01 20:48:12 -0700

    fuiyhuxchxfyvhyikj

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,25 +1,31 @@
-extends CharacterBody2D
+extends CharacterBody3D
 
-const speed = 300
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 func _physics_process(delta):
-	var direction = Vector2.ZERO
-	
-	if Input.is_action_pressed("left"):
-		direction.x -= 1
-	if Input.is_action_pressed("right"):
-		direction.x += 1
-	if Input.is_action_pressed("down"):
-		direction.y += 1
-	if Input.is_action_pressed("up"):
-		direction.y -= 1
-	
-	direction = direction.normalized()
-	
-	velocity = speed * direction
-	print(velocity)
-	
-	move_and_slide()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 
-func _draw():
-	draw_rect(Rect2(Vector2.ZERO,Vector2.ONE * 20), Color.REBECCA_PURPLE)
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
