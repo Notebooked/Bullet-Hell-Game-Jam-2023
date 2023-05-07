f90f27a8c4ff7f3d38df9f5cbc8e8fb4a29cd62d
juicetanz <66227184+juicetanz@users.noreply.github.com>
2023-05-06 20:44:08 -0700

    retry

--- Scripts/Pause.gd
+++ Scripts/Pause.gd
@@ -3,18 +3,29 @@ extends Control
 func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_ESCAPE:
-			if not self.visible:
+			if not $Control.visible:
 				get_tree().paused = true
-				self.visible = true
+				$Control.visible = true
 			else:
-				self.visible = false
+				$Control.visible = false
 				get_tree().paused = false
 
+func _on_pause_button_pressed():
+	if not $Control.visible:
+		get_tree().paused = true
+		$Control.visible = true
+	else:
+		$Control.visible = false
+		get_tree().paused = false
+
 func _on_play_button_pressed():
-	self.visible = false
+	$Control.visible = false
 	get_tree().paused = false
 
 func _on_quit_button_pressed():
-	self.visible = false
+	$Control.visible = false
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+
+func _on_retry_button_pressed():
+	get_tree().reload_current_scene()
