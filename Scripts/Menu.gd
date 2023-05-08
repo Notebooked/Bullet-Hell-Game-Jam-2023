2d0d83fd4bff3abf2c894999c9f10e1f447c6057
juicetanz <66227184+juicetanz@users.noreply.github.com>
2023-05-07 22:51:41 -0700

    woah!

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -8,3 +8,6 @@ func _on_quit_button_pressed():
 
 func _on_level_select_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Selection.tscn")
+
+func _on_about_button_pressed():
+	OS.shell_open("https://youthdevstudios.github.io")
